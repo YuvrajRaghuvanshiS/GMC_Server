595fad644b7f6d005f2fe27e10672d7af90b3aeb
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2023-02-16 18:07:46 +0530

    Added cross origin support.

--- code/app.py
+++ code/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from resources.user import UserData
 
 
 app: Flask = Flask(__name__)
+CORS(app, origins=["https://app.gate.iitk.ac.in"])
+# CORS(app, resources={r"/user-data": {"origins": "http://example.com"}})
 api: Api = Api(app)
 
 
